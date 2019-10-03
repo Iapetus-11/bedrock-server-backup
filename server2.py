c2c46bf731cbaf5788be9e1b8cf99c55bd11e181
Iapetus-11 <38477514+Iapetus-11@users.noreply.github.com>
2019-10-03 18:22:42 -0400

    Update server2.py

--- server2.py
+++ server2.py
@@ -3,20 +3,24 @@ import threading
 from time import sleep
 from os import system
 
+#open new process opening bedrock_server.exe
 process = subprocess.Popen('bedrock_server.exe', stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
+#allows for input from the console
 def InputLoop():
     while True:
         inp = input() + "\n"
         process.stdin.write(inp.encode())
         process.stdin.flush()
 
+#output from bedrock_server.exe
 def OutputLoop():
     while True:
         for line in process.stdout:
             line = str(line).replace("b'","")[:-5]
             print(line)
 
+#backing up loop
 def BackupLoop():
     while True:
         toType = "save hold" + "\n"
@@ -29,7 +33,8 @@ def BackupLoop():
         process.stdin.write(toType.encode())
         process.stdin.flush()
         sleep(3600)
-    
+
+#start the threads
 outputt = threading.Thread(target=InputLoop)
 inputt = threading.Thread(target=OutputLoop)
 
